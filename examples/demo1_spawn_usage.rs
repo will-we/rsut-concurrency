cb77f9997faa0abe672a11345f8ba5c040ec32b3
will-we <weirson.lee@gmail.com>
2025-02-21 00:22:06 +0800

    fixed some issues

--- examples/demo1_spawn_usage.rs
+++ examples/demo1_spawn_usage.rs
@@ -5,7 +5,8 @@ fn main() -> anyhow::Result<()> {
         println!("hello world by the thread id {:?}!", thread::current().id());
     });
 
-    println!("this is the main thread id {:?}!", handle.thread().id());
+    //show the main thread id
+    println!("this is the main thread id {:?}!", thread::current().id());
 
     // wait for the thread to finish
     handle.join().map_err(|e| anyhow::anyhow!("{:?}", e))?;
