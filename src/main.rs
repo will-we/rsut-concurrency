49178d924310b3ad86b9b8d1763217355ca03aa0
will-we <weirson.lee@gmail.com>
2025-02-25 22:29:22 +0800

    add tokio basic example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::sync::Arc;
+
+#[tokio::main]
+async fn main() {
+    let data = Arc::new(vec![1, 2, 3]);
+
+    let handle = tokio::spawn({
+        let data = Arc::clone(&data);
+        async move {
+            println!("子任务访问数据: {:?}", data);
+        }
+    });
+    handle.await.unwrap();
 }
